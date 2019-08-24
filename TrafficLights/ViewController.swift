cf893ed050ba4bb3096ec15aaf00abc36a27123a
Bogdan <bogdanguk@gmail.com>
2019-08-25 00:00:54 +0300

    created interface with constrains and create some IBOutlets and IBActions for simple reaction game

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trafficLightImage: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var startTimer = Timer()
+    var scoreTimer = Timer()
+    
+    var timerInt = 0
+    var scoreInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func startGameAction(_ sender: Any) {
+    
+        
+        
+    }
+    
 
 
 }
